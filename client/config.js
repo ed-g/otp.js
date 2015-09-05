24046f402d181e36445edbef95a517a9c13a25f2
ed-g <edward.groth@gmail.com>
2015-09-05 13:16:14 -0700

    cherriots toolbar

--- client/config.js
+++ client/config.js
@@ -1,11 +1,19 @@
 window.OTP_config = {
-  initLatLng: [38.880148, -77.105933],
 
-  osmMapKey: 'conveyal.ikck6888', // temporary -- do not use in production, provide your own
-  aerialMapKey: 'conveyal.map-a3mk3jug', // unset
+  initLatLng: [44.9473,-123.0564],
+  // initial zoom: , 13
 
-  otpApi: 'http://192.168.59.103:8080/otp/routers/',
+  osmMapKey: 'conveyal.map-jc4m5i21', 
+  aerialMapKey: 'conveyal.map-a3mk3jug', 
+
+  // otpApi: 'http://maps.cherriots.org:8080/opentripplanner-api-webapp/ws/',
+  // otpApi: 'http://maps.cherriots.org/opentripplanner-api-webapp/ws/',
+ 
+  // moving-forward router
+  otpApi: 'http://maps.cherriots.org/moving-forward/otp/routers/',  
   esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
 
+  simplecoderApi: 'http://maps.cherriots.org:9998',
+
   reverseGeocode: true
-}
+};
